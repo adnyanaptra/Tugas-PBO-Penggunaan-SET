a1c20040aa70d9945efe2612683ea48e1e9cacff
adnyanaptra <100030606+adnyanaptra@users.noreply.github.com>
2022-03-16 12:54:45 +0800

    Add files via upload

---
+++ map.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class set {
+        public static void main (String[] args){
+        Set<String> set = new LinkedHashSet<String>();
+
+        //Menyimpan data no telpon
+        set.add("08123543");
+        set. add("08763789");
+        set.add("08134523");
+        System.out.println(set);
+
+        }
+        }
