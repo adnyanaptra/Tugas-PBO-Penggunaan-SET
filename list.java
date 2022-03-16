a8577de396c0236d6081a0a1476b4fec444e2965
adnyanaptra <100030606+adnyanaptra@users.noreply.github.com>
2022-03-16 12:59:55 +0800

    Add files via upload

---
+++ list.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class set (
+        public static void main (String[] args) {
+    List<String> list = new ArrayList<String>();
+        //Menampilkan urutan nama mahasiwa
+        List.add("Andi");
+        list.add("Dono");
+        list.add("Antok");
+        System.out.println(List);
+
+}
+}
