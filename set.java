5408ac02993881e60c217540b04631fc171ea21d
adnyanaptra <100030606+adnyanaptra@users.noreply.github.com>
2022-03-16 12:42:04 +0800

    Add files via upload

---
+++ set.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class set {
+    public static void main (String[] args){
+        Map<String, String> map = new HashMap<String, String>();
+
+        //Menampilkan urutan nim dan nama mahasiwa
+        map.put("2104564", "Andi");
+        map.put("2103456", "Dono");
+        map.put("2109858", "Antok");
+
+        System.out.println(map);
+    }
+}
